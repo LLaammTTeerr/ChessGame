d8738e3bdccc65439687888094f59fbb3d88e2b6
LeBon <LeBon@DESKTOP-BKS0M9L>
2024-12-16 21:44:03 +0700

    abc

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include "libs/ChessGame.h"
-#include <QtWidgets/QApplication>
-
-int main(int argc, char* argv[]) {
-    QApplication a(argc, argv);
-    ChessGame w;
-    w.show();
-    return a.exec();
-}
